6b7dbcb3d7a681d9eb0b91d3ca59de789a44312e
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-10-09 20:58:01 +0500

    fib_series_ques

---
+++ Recursion/fibonacciSeries.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int fib(int n)
+{
+  if (n == 0)
+    return 0;
+  if (n == 1)
+    return 1;
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+
+  int n;
+  cout << "Enter the value of N: " << endl;
+  cin >> n;
+
+  cout << fib(n);
+}
