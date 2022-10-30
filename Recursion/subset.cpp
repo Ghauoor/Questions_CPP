f6f48b5a5a10c2c051766e3d40c8fc7da85ffc1a
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-10-30 13:11:24 +0500

    subset

--- Recursion/subset.cpp
+++ Recursion/subset.cpp
@@ -7,10 +7,8 @@ void solve(vector<int> nums, vector<int> output, int index, vector<vector<int>> 
 {
 
   // Base case
-
   if (index >= nums.size())
   {
-
     ans.push_back(output);
     return;
   }
@@ -31,8 +29,4 @@ vector<vector<int>> subsets(vector<int> &nums)
 
   solve(nums, output, index, ans);
   return ans;
-}
-
-int main()
-{
 }
