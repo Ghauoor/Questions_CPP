733110dc36915a1e643c3384e28b48c1681bd870
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-11-29 13:31:02 +0500

    OOP-Oprator-oberloading

--- OOP/polymorphism.cpp
+++ OOP/polymorphism.cpp
@@ -16,9 +16,41 @@ public:
     }
 };
 
+class B
+{
+public:
+    int a;
+    int b;
+
+public:
+    int add()
+    {
+        return a + b;
+    }
+
+    void operator+(B &obj)
+    {
+        int value1 = this->a;
+        int value2 = obj.a;
+        cout << "output " << value2 - value1 << endl;
+
+        cout << "Hello Naruto" << endl;
+    }
+
+    void operator()()
+    {
+        cout << "main Bracket hu " << this->a << endl;
+    }
+};
+
 int main()
 {
     A obj;
     obj.sayHello();
-    
+
+    B obj1, obj2;
+    obj1.a = 4;
+    obj2.a = 7;
+    obj1 + obj2;
+    obj1();
 }
