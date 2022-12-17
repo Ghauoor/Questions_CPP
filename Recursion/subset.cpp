f59b57a940d639094b669f4311f536603429f343
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-12-17 17:26:52 +0500

    Singly Linked List

--- Recursion/subset.cpp
+++ Recursion/subset.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 void solve(vector<int> nums, vector<int> output, int index, vector<vector<int>> &ans)
 {
-  // Base case
+  //* Base case
   if (index >= nums.size())
   {
     ans.push_back(output);
     return;
   }
-  // exclude
+  //* exclude
   solve(nums, output, index + 1, ans);
 
-  // include
+  //* include
   int element = nums[index];
   output.push_back(element);
   solve(nums, output, index + 1, ans);
