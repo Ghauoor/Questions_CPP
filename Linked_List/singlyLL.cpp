7a712e4c773b9d5e26ed3590357e7f9070924c13
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-01-10 03:02:03 +0500

    Floyd Cycle Detection Algorithm

--- Linked_List/singlyLL.cpp
+++ Linked_List/singlyLL.cpp
@@ -156,6 +156,33 @@ bool detectLoop(Node *head)
     return false;
 }
 
+// floyed loop detection
+
+bool floyedDetectLoop(Node *head)
+{
+    if (head == NULL)
+        return false;
+
+    Node *slow = head;
+    Node *fast = head;
+
+    while (slow != NULL && fast != NULL)
+    {
+        fast = fast->next;
+
+        if (fast != NULL)
+        {
+            fast = fast->next;
+        }
+        slow = slow->next;
+
+        if (slow == fast)
+            return slow;
+    }
+
+    return false;
+}
+
 int main()
 {
     // created a new node
