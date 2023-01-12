d41bdfc2b45db5ba363a84bebb8f56f11d1c3dbf
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-01-12 05:33:22 +0500

    Remove Duplicates From Linked List

--- Linked_List/singlyLL.cpp
+++ Linked_List/singlyLL.cpp
@@ -232,6 +232,36 @@ Node *removeLoop(Node *head)
     return head;
 }
 
+// Remove Duplicates in SLL
+
+Node *uniqueSortedList(Node *head)
+{
+    // empty List
+    if (head == NULL)
+        return NULL;
+
+    // non empty list
+    Node *curr = head;
+
+    while (curr != NULL)
+    {
+
+        if ((curr->next != NULL) && curr->data == curr->next->data)
+        {
+            Node *next_next = curr->next->next;
+            Node *nodeToDelete = curr->next;
+            delete (nodeToDelete);
+            curr->next = next_next;
+        }
+        else // not equal
+        {
+            curr = curr->next;
+        }
+    }
+
+    return head;
+}
+
 int main()
 {
     // created a new node
