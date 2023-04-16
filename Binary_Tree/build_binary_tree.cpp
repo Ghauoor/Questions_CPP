2e28431687dfdcf4101265f92f4926c26828d1df
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-04-17 02:51:35 +0500

    All Four types Trees Traversals

--- Binary_Tree/build_binary_tree.cpp
+++ Binary_Tree/build_binary_tree.cpp
@@ -79,6 +79,46 @@ void levelOrderTraversal(Node *root)
     }
 }
 
+// inOrder Traversal --> (LNR)
+
+void inorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+// PreOrder Traversal
+
+void preorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// PostOrder traversal
+void postorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
 int main()
 {
     Node *root = NULL;
@@ -89,5 +129,21 @@ int main()
 
     // Level order traversal
     levelOrderTraversal(root);
+
+    // InOrder traversal
+    cout << endl
+         << "In Order Traversal is: ";
+    inorder(root);
+
+    // Pre Order Traversal
+    cout << endl
+         << "Pre Order Traversal is: ";
+    preorder(root);
+
+    // PostOrder traversal
+    cout << endl
+         << "Post Order Traversal is: ";
+    postorder(root);
+
     return 0;
 }
