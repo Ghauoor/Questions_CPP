d338f813ee3cba07b8f94336783c8437663a672d
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-06-03 02:42:08 +0500

    103. Binary Tree Zigzag Level Order Traversal

--- Binary_Tree/zig_zag_traversal.cpp
+++ Binary_Tree/zig_zag_traversal.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 //! https://practice.geeksforgeeks.org/problems/zigzag-tree-traversal/1
+//?https://takeuforward.org/data-structure/zig-zag-traversal-of-binary-tree/
 struct Node
 {
     int data;
@@ -61,3 +62,21 @@ public:
         return result;
     }
 };
+int main()
+{
+    int i, j;
+    Node *root = new Node(3);
+    root->left = new Node(9);
+    root->right = new Node(20);
+    root->right->left = new Node(15);
+    root->right->right = new Node(7);
+    vector<int> ans;
+    ans = Solution().zigZagTraversal(root);
+    cout << "Zig Zag Traversal of Binary Tree" << endl;
+    for (i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
